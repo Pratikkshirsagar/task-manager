851ff92bc281d45af726df25b6948c82d0b89449
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-20 23:47:51 +0530

    inserting doc done

--- mongodb.js
+++ mongodb.js
@@ -14,18 +14,61 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne(
-      {
-        name: 'Pratik',
-        age: 24,
-      },
-      (error, result) => {
-        if (error) {
-          return console.log('Unable to insert user');
-        }
+    // db.collection('users').insertOne(
+    //   {
+    //     name: 'Pratik',
+    //     age: 24,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('Unable to insert user');
+    //     }
 
-        console.log(result.ops);
-      }
-    );
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       name: 'Jen',
+    //       age: 28,
+    //     },
+    //     {
+    //       name: 'sam',
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log('unable to insert documents');
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection('users').insertMany(
+    //   [
+    //     {
+    //       description: 'Testing the mongodb',
+    //       compleated: true,
+    //     },
+    //     {
+    //       description: 'Learning Node',
+    //       compleated: true,
+    //     },
+    //     {
+    //       description: 'Learn the basics',
+    //       compleated: true,
+    //     },
+    //   ],
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.log('Unable to insert document');
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
