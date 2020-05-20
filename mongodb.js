6569b3948658dcecc3c49b974e284b0d87b3f256
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-20 22:58:28 +0530

    inserted one document

--- mongodb.js
+++ mongodb.js
@@ -11,5 +11,21 @@ MongoClient.connect(
     if (error) {
       return console.log('Unable to connect to database!');
     }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne(
+      {
+        name: 'Pratik',
+        age: 24,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert user');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
