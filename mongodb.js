5702ed58bf18264974dedac4bafeb5275ae21ec7
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-21 12:42:46 +0530

    get single doc from db.

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,8 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -14,61 +17,12 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne(
-    //   {
-    //     name: 'Pratik',
-    //     age: 24,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('Unable to insert user');
-    //     }
+    db.collection('users').findOne({ name: 'Pratik' }, (error, user) => {
+      if (error) {
+        return console.log('Unable to fetch user');
+      }
 
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection('users').insertMany(
-    //   [
-    //     {
-    //       name: 'Jen',
-    //       age: 28,
-    //     },
-    //     {
-    //       name: 'sam',
-    //       age: 27,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('unable to insert documents');
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection('users').insertMany(
-    //   [
-    //     {
-    //       description: 'Testing the mongodb',
-    //       compleated: true,
-    //     },
-    //     {
-    //       description: 'Learning Node',
-    //       compleated: true,
-    //     },
-    //     {
-    //       description: 'Learn the basics',
-    //       compleated: true,
-    //     },
-    //   ],
-    //   (err, result) => {
-    //     if (err) {
-    //       return console.log('Unable to insert document');
-    //     }
-
-    //     console.log(result.ops);
-    //   }
-    // );
+      console.log(user);
+    });
   }
 );
